c96a29ce287fd24eb71c959747bf3c2750330b1a
fernandoallisson <fernando.alisson@ufpe.br>
2023-04-16 17:23:36 -0300

    Restabelecendo algumas classes e ids, criação de função para sortear as cores adicionando a cor preta como inicial.

--- script.js
+++ script.js
@@ -1,23 +1,45 @@
-// ------------------------------------------[EX01]-----------------------------------------------
 // Aqui vou criar uma função para gerar cores aleatórias e posteriormente usar em uma das divs criadas. 
 
-const GerarCorAleatória = () => {
+const gerCorAleatoria = () => {
   const red = Math.floor(Math.random() * 256);
   const green = Math.floor(Math.random() * 256);
   const blue = Math.floor(Math.random() * 256);
   return (`rgb(${red}, ${green}, ${blue})`)
 }
-const corBranca = 'rgb(255, 255, 255)'
-// As cores geradas. Agora vou pegar as divs das cores que têm a classe color para adicionar a cada uma, uma das cores assim que recarregar a página, menos a cor branca.
+const corBranca = 'rgb(255, 255, 255)';
+// As cores geradas. Agora vou pegar as divs das cores que têm a classe color para adicionar a cada uma, uma das cores assim que recarregar a página, menos a cor branca. E o primeiro elemento com a cor Preta.
 
 const divCor = document.querySelectorAll('.color');
 for (const div of divCor) {
-  div.style.backgroundColor = GerarCorAleatória() != corBranca ? GerarCorAleatória(): GerarCorAleatória();
+  div.style.backgroundColor = gerCorAleatoria() != corBranca ? gerCorAleatoria(): gerCorAleatoria();
+  divCor[0].style.backgroundColor = 'black'
 }
-// As cores foram geradas automaticamente, então eu vou atribuir ao botão sortear, as cores em formato aleatório. 
+// As cores foram geradas automaticamente, então eu vou atribuir ao botão sortear, as cores em formato aleatório. E adicionando à primeira div a cor preta.
+
+const sortear = document.querySelector('#rand');
+
+sortear.addEventListener('click', () => {
+  divCor.forEach(div => {
+    let corGerada = gerCorAleatoria();
+    if (corGerada !== corBranca) {
+      div.style.backgroundColor = corGerada;
+      divCor[0].style.backgroundColor = 'black'
+    } else {
+      corGerada = gerCorAleatoria();
+      div.style.backgroundColor = corGerada;
+    }
+  });
+});
 
 
-// console.log(divCor);
+
+
+
+
+
+
+
+
 
 // divCor.forEach(div => {
 //   div.addEventListener('click', () => {
