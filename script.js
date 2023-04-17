9a72b5c020e9109ca1783a79185d982feabe1f15
fernandoallisson <fernando.alisson@ufpe.br>
2023-04-17 15:58:45 -0300

    Oragnizando o código e adicionando a função para salvar localmente

--- script.js
+++ script.js
@@ -1,34 +1,56 @@
-// Aqui vou criar uma função para gerar cores aleatórias e posteriormente usar em uma das divs criadas. 
+window.addEventListener('load', () => {
+  let salvarPaleta = ['rgb(0,0,0)']; // Nessa constante eu vou adicionar o resto das cores que faltam para completar as quatro cores.
+  const coresSalvas = JSON.parse(localStorage.getItem('colorPalette'));
+  const corBranca = 'rgb(255, 255, 255)';
+  const divCor = document.querySelectorAll('.color'); // DIV IMPORTANTE PARA PEGAR OS ELEMENTOS NO HTML. ESSES SÃO OS BOTÕES DAS CORES.
+  const sortear = document.querySelector('#button-random-color'); // Pega o elemento 'botão' de Sortear lá no HTML
+  
 
+  
+
+// functions ---------------------------------------------------------
+// Aqui vou criar uma função para gerar cores aleatórias e posteriormente usar em uma das funções criadas. 
 const gerCorAleatoria = () => {
   const red = Math.floor(Math.random() * 256);
   const green = Math.floor(Math.random() * 256);
   const blue = Math.floor(Math.random() * 256);
   return (`rgb(${red}, ${green}, ${blue})`)
 }
-const corBranca = 'rgb(255, 255, 255)';
-// As cores geradas. Agora vou pegar as divs das cores que têm a classe color para adicionar a cada uma, uma das cores assim que recarregar a página, menos a cor branca. E o primeiro elemento com a cor Preta.
 
-const divCor = document.querySelectorAll('.color');
-for (const div of divCor) {
-  div.style.backgroundColor = gerCorAleatoria() != corBranca ? gerCorAleatoria(): gerCorAleatoria();
-  divCor[0].style.backgroundColor = 'black'
+
+
+// Aqui eu quero criar um loop para adicionar as cores dos elementos que eu salvei no localStorage e aplicar a cada um dos itens caso haja alguma cor salva. 
+if(coresSalvas) {
+  for(let i = 0; i <coresSalvas.length; i += 1) {
+    divCor[i].style.backgroundColor = coresSalvas[i]
+  }
+} else {
+  // Com as cores geradas. Agora vou pegar as divs das cores que têm a classe '.color' para adicionar a cada uma, uma das cores assim que recarregar a página, menos a cor branca. E o primeiro elemento com a cor Preta.
+  for (let index = 1; index < divCor.length; index += 1) {
+    let corGerada = gerCorAleatoria() != corBranca ? gerCorAleatoria(): gerCorAleatoria();
+    divCor[index].style.backgroundColor = corGerada;
+    divCor[0].style.backgroundColor = 'rgb(0,0,0)';
+    salvarPaleta.push(corGerada)
+  }
+  localStorage.setItem('colorPalette', JSON.stringify(salvarPaleta))
 }
-// As cores foram geradas automaticamente, então eu vou atribuir ao botão sortear, as cores em formato aleatório. E adicionando à primeira div a cor preta.
 
-const sortear = document.querySelector('#button-random-color');
-
+// As cores foram geradas automaticamente ou restauradas, então eu vou atribuir ao botão sortear, as cores em formato aleatório. E adicionando à primeira div a cor preta.
 sortear.addEventListener('click', () => {
-  divCor.forEach(div => {
-    let corGerada = gerCorAleatoria();
-    if (corGerada !== corBranca) {
-      div.style.backgroundColor = corGerada;
-      divCor[0].style.backgroundColor = 'black'
-    } else {
-      corGerada = gerCorAleatoria();
-      div.style.backgroundColor = corGerada;
-    }
-  });
+  salvarPaleta = ['rgb(0,0,0)']
+  for (let index = 1; index < divCor.length; index += 1) {
+    let corGerada = gerCorAleatoria() != corBranca ? gerCorAleatoria(): gerCorAleatoria();
+    divCor[index].style.backgroundColor = corGerada;
+    divCor[0].style.backgroundColor = 'rgb(0,0,0)';
+    salvarPaleta.push(corGerada);
+  }
+  localStorage.setItem('colorPalette', JSON.stringify(salvarPaleta))
 });
 
 
+
+
+
+
+
+});
